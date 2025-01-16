464c9d9a57b858cd6066c2670db8b7c906dd87f1
Swati <swatijaiswal90090@gmail.com>
2025-01-16 15:32:36 +0530

    Add

--- code_03.py
+++ code_03.py
@@ -1,3 +1,54 @@
-file = open('swati.txt', 'r')
+'''file = open('swati.txt', 'r')
 content = file.read()
 print(content)
+
+
+file = open('swati.txt', 'r')
+#content = file.read()
+line = file.readline()
+print(line)
+
+
+file = open('swati.txt', 'r')
+#content = file.read()
+lines = file.readlines()
+print(lines)
+
+
+'''
+
+
+'''
+file = open('swati.txt', 'r')
+file = open('swati.txt', 'w')
+file.write('Hello, Swati\n')
+content = file.read()
+print(content)
+
+file = open('swati.txt', 'r')
+# file = open('swati.txt', 'w')
+#file.write('Hello, Swati\n')
+content = file.read()
+print(content) 
+'''
+
+'''
+
+file = open('swati.txt', 'a')
+file.write('\nHello, tush\n')
+
+file = open('swati.txt', 'r')
+#file = open('swati.txt', 'a')
+#file.write('\nHello, tush\n')
+content = file.read()
+print(content) 
+
+
+
+
+file.close()
+'''
+
+
+
+import os
