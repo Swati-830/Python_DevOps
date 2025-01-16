1dd2df76f59ce1d7d4d19fb3ec900364ab7abf3a
Swati <swatijaiswal90090@gmail.com>
2025-01-16 14:47:28 +0530

    add

---
+++ code_02.py
@@ -0,0 +1,103 @@
+'''
+## Day ----> 1
+
+x=29
+y=8
+
+addition = x+y
+subtraction = x-y
+Multiplication = x*y
+Division = x/y
+
+# print(addition, subtraction, Multiplication, Division)
+
+print("addition is :", addition)
+print("subtraction is :", subtraction)
+print("Multiplication is :", Multiplication)
+print("Division is :", Division)
+
+# print("addition is :", addition, "subtraction is :", subtraction, "Multiplication is :", Multiplication, "Division is :", Division)
+
+## Day ----> 2
+my_list = [1,2,3,4,5, 'Tushar', 'Swati']
+
+print(my_list)
+print(my_list[5])
+my_list[4] = 'tush'
+print(my_list)
+
+my_tuple = (1,2,3,4,5, 'Tushar', 'Swati')
+print(my_tuple)
+print(my_tuple[5])
+# my_tuple[4] = 234
+print(my_tuple)
+
+person = {'name':'John', 'age':30, 'city':'New York','gender':'male'}
+print(person)
+print(person['name'])
+person['education'] = 'Btech'
+print(person)
+
+my_set = {1,2,3,4,55,3,4,5,2,4,5,2,5,3,2,7,6,}
+print(my_set)
+
+'''
+
+'''
+# Integer
+integer_var = 42
+print("Integer Variable:", integer_var)
+
+# Float
+float_var = 3.14
+print("Float Variable:", float_var)
+
+# String
+string_var = "Hello, World!"
+print("String Variable:", string_var)
+
+# List
+list_var = [1, 2, 3, 4, 5]
+print("List Variable:", list_var)
+
+# Tuple
+tuple_var = (10, 20, 30, 40, 50)
+print("Tuple Variable:", tuple_var)
+
+# Dictionary
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary Variable:", dict_var)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+print("Set Variable:", set_var)
+
+
+# Integer and Float operations
+result = integer_var + float_var
+print("Integer + Float:", result)
+
+# String concatenation
+new_string = string_var + " Have a nice day!"
+print("Concatenated String:", new_string)
+
+# List manipulation
+list_var.append(6)
+list_var.remove(2)
+print("Modified List:", list_var)
+
+# Dictionary operations
+dict_var['d'] = 4
+del dict_var['a']
+print("Modified Dictionary:", dict_var)
+
+# Set operations
+set_var.add(6)
+set_var.remove(3)
+print("Modified Set:", set_var)
+
+'''
+
+
+
+
---
+++ code_03.py
@@ -0,0 +1,3 @@
+file = open('swati.txt', 'r')
+content = file.read()
+print(content)
